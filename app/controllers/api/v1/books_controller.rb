a9bc9e7fc33a87bfb9863153a04a922c94254fdc
nazeh <ar.nazeh@gmail.com>
2019-11-13 16:49:45 +0300

    implemented destroy and create methods in Books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,14 +7,44 @@ module Api
         books = Book.order('created_at DESC')
         render json: {
           status: 'SUCCESS',
-          message: 'Load books',
+          message: 'Loaded books',
           data: books
         }, status: :ok
       end
 
-      def create; end
+      def create
+        book = Book.new(book_params)
 
-      def destroy; end
+        if book.save
+          render json: {
+            status: 'SUCCESS',
+            message: 'Saved book',
+            data: book
+          }, status: :ok
+        else
+          render json: {
+            status: 'ERROR',
+            message: 'Book not saved',
+            data: book.errors
+          }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        render json: {
+          status: 'SUCCESS',
+          message: 'Deleted book',
+          data: book
+        }, status: :ok
+      end
+
+      private
+
+      def book_params
+        params.permit(:title, :author)
+      end
     end
   end
 end
