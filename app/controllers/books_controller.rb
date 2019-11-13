67a52cb778d5abdeaa9c42e3d767bdd988dc44c9
nazeh <ar.nazeh@gmail.com>
2019-11-13 15:29:55 +0300

    generate Books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class BooksController < ApplicationController
+  def create; end
+
+  def index; end
+
+  def destroy; end
+end
--- db/migrate/20191113122207_create_books.rb
+++ db/migrate/20191113122207_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
-      t.String :title
-      t.String :author
-      t.String :category
+      t.string :title
+      t.string :author
+      t.string :category
 
       t.timestamps
     end
